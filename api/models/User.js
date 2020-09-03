a9d7d8a1c4444045a07e36b7bd32d3a170c4e2b6
sandeep-jft <sandeep.singh@jellyfishtechnologies.com>
2020-09-03 13:43:11 +0530

    add model

---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+module.exports={
+    attributes:{
+        username:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    }
+}
